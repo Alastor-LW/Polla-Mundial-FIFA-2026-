c4e5ce93cc0db9c142c0a797ff855af900123c9e
Alastor-LW <6n7mcckmdg@privaterelay.appleid.com>
2026-05-22 00:11:13 -0500

    Delete supabase_update.sql

--- supabase_update.sql
+++
@@ -1,6 +0,0 @@
--- Actualización de tabla settings para soportar bloqueo de fases
-ALTER TABLE settings ADD COLUMN IF NOT EXISTS phase1_locked boolean default false;
-ALTER TABLE settings ADD COLUMN IF NOT EXISTS phase2_locked boolean default false;
-
--- Actualizar el registro existente
-UPDATE settings SET phase1_locked = false, phase2_locked = false WHERE id = 1;
